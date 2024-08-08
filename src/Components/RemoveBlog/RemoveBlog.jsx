b2b413898da086b8a7cb3e8997c97ca18f65c8af
Ridoy sharif <wdridoy11@gmail.com>
2024-08-08 20:27:10 +0600

    handle favorite blog remove button create successful

--- src/Components/RemoveBlog/RemoveBlog.jsx
+++ src/Components/RemoveBlog/RemoveBlog.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { FaHeart, FaRemoveFormat, FaTruck } from 'react-icons/fa'
+import { FaHeart, FaTrash} from 'react-icons/fa'
+import Swal from 'sweetalert2'
 
 const RemoveBlog = ({blog}) => {
-  console.log(blog)
+
+  const handleFavoriteRemove=(id)=>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You will delete your favorite blog",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes'
+    }).then((result)=>{
+      if(result.isConfirmed){
+          fetch(`${process.env.REACT_APP_API_URL}/favorite-blog/${id}`,{
+            method:"DELETE",
+            headers:{
+              "content-type":"application/json"
+            }
+          })
+          .then((res)=>res.json())
+          .then((data)=>console.log(data))
+      }
+    })
+
+  }
+
   return (
     <>
-        <button className='blog-favorite hover:bg-red-600 hover:border-red-600'>
-            <FaHeart className='text-xs'></FaHeart>
+        <button onClick={()=>handleFavoriteRemove(blog._id)} className='blog-favorite bg-blog-primary text-white hover:bg-red-600 hover:border-red-600'>
+            {/* <FaHeart className='text-xs'></FaHeart> */}
+            <FaTrash className='text-lg' />
         </button>
     </>
   )
